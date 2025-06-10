dc4b27b584a1a1b22be8162e689f2443e43ed3b4
bishnu05 <101577876+bishnu05@users.noreply.github.com>
2025-06-11 00:22:45 +0530

    context with localstorage

--- todocontext/src/App.jsx
+++ todocontext/src/App.jsx
@@ -3,7 +3,17 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-3xl fonnt-bold underline">Hello World!</h1>
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rouded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-ceter mb-8 mt-2">
+            Manage your Todos
+          </h1>
+          <div className="mb-4">{/* Todo from goes here */}</div>
+          <div className="flex flex-wrap gap-y-3">
+            {/* Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
