77f7d1ea86e265677e52fcb903c3bf94d6260f1c
bishnu05 <101577876+bishnu05@users.noreply.github.com>
2025-06-25 23:41:34 +0530

    input & button components created

--- MegaBlog/src/conf/conf.js
+++ MegaBlog/src/conf/conf.js
@@ -1,6 +1,6 @@
 const conf = {
-    appwriteUrl: "https://cloud.appwrite.io/v1",
-    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteUrl: "https://fra.cloud.appwrite.io/v1",
+    appwriteProjectId: "68506eff00146106edcd",
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
